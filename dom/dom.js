fab2ad84406f82eddda3ae4eff8a954eee4cf3ff
xiuye <xiuye_engineer@outlook.com>
2019-05-05 17:02:40 +0800

    new api and fix bugs

--- dom/dom.js
+++ dom/dom.js
@@ -1,8 +1,9 @@
 /**
  * 
  */
+export default class Dom{
 
-export default class Dom {
+
     static DOM = new Dom(document); 
 
     constructor(node) {
@@ -17,7 +18,29 @@ export default class Dom {
             this.node.remove();
     }
 
-    
+    value(v){
+        // undefined == null : true
+        // undefined === null : false
+        // if(!!v||v=='' || v === null){
+        //     this.node.value = v;
+        //     return;
+        // }
+        // if(v === undefined){
+        //     this.node.value = '';
+        //     return;
+        // }
+        if(arguments.length>0){
+            if(v !== undefined ){
+                this.node.value = v;
+            }
+            else{
+                this.node.value = '';
+            }
+        }
+        
+
+        return this.node.value;
+    }
 
     rmChild(node){
         this.node.removeChild(node);
@@ -62,6 +85,7 @@ export default class Dom {
 
         if(arguments.length==0){
             this.trigger('click');
+            return;
         }
 
         if(only){
@@ -81,9 +105,18 @@ export default class Dom {
         
     }
 
-
     static d(node){
-        
+        // 用系统自带的选择器api，就不用自己写状态机了
+        let elements = document.querySelectorAll(node);
+        if(elements.length==0){
+            return;
+        }
+        if(elements.length==1){
+            return new Dom(elements[0]);
+        }
+        if(elements.length>1){
+            return new DomList(elements);
+        }
     } 
     static byId(id) {
         return new Dom(document.getElementById(id));
@@ -105,4 +138,100 @@ export default class Dom {
 
 }
 Object.freeze(Dom);
+class DomList{
+    constructor(elements){
+    	let domList = [];
+        let i=0;
+    	elements.forEach((d)=>{
+            domList.push(new Dom(d));
+            this[i++] = d;
+        });
+        this.length = i-1;
+
+        this.domList = domList;
+        
+    }
+
+    item(index){
+        return this.domList[index];
+    }
+
+    forEach(callback){
+    	this.domList.forEach(callback);
+    }
+    
+    rmSelf(){
+        this.forEach(node=>node.rmSelf());
+    }
+
+    value(v){
+        if(arguments.length>0){
+            this.forEach(node=>node.value(v));
+        }
+        let vs = [];
+        this.forEach(node=>{if(arguments.length>0){vs.push(node.value())}});
+        return vs;
+    }
+
+    rmChild(n){
+        this.forEach(node=>node.rmChild(n));
+    }
+
+    append(n){
+        this.forEach(node=>node.append(n));
+    }
+
+    set(attr,value){
+        this.forEach(node=>node.set(attr,value));
+    }
+
+    get(attr){
+    	let vs = [];
+    	this.forEach(node=>vs.push(node.get(attr)));
+        return vs;
+    }
+
+    rm(attr){
+        this.forEach(node=>node.rm(attr));
+    }
+
+
+
+    on(e, callback) {
+        if (arguments.length < 2) {
+            throw "error:this method has two parameters.";
+        }
+        this.forEach(node=>node.on(e,callback));
+    }
+    off(e, callbck) {
+        if (arguments.length < 2) {
+            throw "error:this method has two parameters.";
+        }
+        this.forEach(node=>node.off(callback));
+    }
+
+    trigger(e){
+        this.forEach(node=>node.trigger(e));
+    }
+
+    click(callback,only=false){
+
+        if(arguments.length==0){
+            this.trigger('click');
+            return;
+        }
+
+        if(only){
+            this.forEach(node=>node.onclick=callback);
+            return;
+        }
+        this.on("click",callback);
+    }
+
+   
+
+}
+Object.freeze(DomList);
+// export default Dom;
+// export Dom;
 

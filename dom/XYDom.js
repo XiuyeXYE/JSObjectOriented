549a7b3aa473157164ca2ff895de5c5a0d27819c
xiuye <xiuye_engineer@outlook.com>
2019-05-17 12:21:24 +0800

    low version compatibility

---
+++ dom/XYDom.js
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+(function( global, factory ) {
+
+	if ( typeof module === "object" && typeof module.exports === "object" ) {
+		module.exports = global.document ?
+			factory( global, true ) :
+			function( w ) {
+				if ( !w.document ) {
+					throw new Error( "XYDom requires a window with a document" );
+				}
+				return factory( w );
+			};
+	} else {
+		factory( global );
+	}
+
+// Pass this if window is not defined yet
+}(typeof window !== "undefined" ? window : 
+	this, 
+	function( window, noGlobal ) {
+	var document = window.document;
+	function dom(node){
+		this.node = node;
+	};
+	
+	dom.prototype.attr=function(k,v){
+		if(arguments.length==0){
+			throw "less than one parameter!";
+		}
+		else if(arguments.length==1){
+			return this.node.getAttribute(k);
+		}
+		else if(arguments.length==2){
+			this.node.setAttribute(k,v);
+		}
+	};
+	
+	//为了解决和jQuery等框架的冲突，必须是函数，真操蛋！！！
+	var xy = function(){};
+	xy.__proto__.byId=function(id){
+		return new dom(document.getElementById(id));
+	}
+	
+	window.xy = xy;
+	return xy;
+}));
+

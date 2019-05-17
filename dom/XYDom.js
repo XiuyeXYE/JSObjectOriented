129b2a4d28e425a29a6a1e89bb3f32bf4caa178e
xiuye <xiuye_engineer@outlook.com>
2019-05-17 12:42:09 +0800

    add css

--- dom/XYDom.js
+++ dom/XYDom.js
@@ -36,7 +36,17 @@
 			this.node.setAttribute(k,v);
 		}
 	};
-	
+	dom.prototype.css=function(k,v){
+		if(arguments.length==0){
+			throw "less than one parameter!";
+		}
+		else if(arguments.length==1){
+			return this.node.style[k];
+		}
+		else if(arguments.length==2){
+			this.node.style[k] = v;
+		}
+	}
 	//为了解决和jQuery等框架的冲突，必须是函数，真操蛋！！！
 	var xy = function(){};
 	xy.__proto__.byId=function(id){

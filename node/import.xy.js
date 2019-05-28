faefc7c833db2eb6e0655376d62e6b25f5c64326
xiuye <xiuye_engineer@outlook.com>
2019-05-28 12:33:06 +0800

    node execute

---
+++ node/import.xy.js
@@ -0,0 +1,5 @@
+var m = require('../core/xy.min.js');
+console.log(m.xy);
+console.log(Object.getOwnPropertyNames(m.xy.EMPTY_VALUES));
+console.log(Object.getOwnPropertyDescriptor(m.xy.EMPTY_VALUES,'EMPTY_ARRAY'));
+

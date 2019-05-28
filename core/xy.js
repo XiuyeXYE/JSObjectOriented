d60ac0e9a9643c4595dc779db8157254305a0e49
xiuye <xiuye_engineer@outlook.com>
2019-05-28 08:25:28 +0800

    changes

--- core/xy.js
+++ core/xy.js
@@ -2,7 +2,7 @@
  * my dom handler API class level function is not equal with obj function. class
  * function cannot be called by obj!!! obj function cannot be called by class!!!
  */
-;(function (global, factory) {
+; (function (global, factory) {
     factory(global);
 }(typeof window !== "undefined" ? window : this, function (window) {
 
